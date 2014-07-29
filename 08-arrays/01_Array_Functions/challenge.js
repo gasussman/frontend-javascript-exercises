dcb4d7d2fd741f79c58002b92c6d5d116cbb38c5
Garrett Sussman <gasussman@gmail.com>
2014-07-29 10:09:23 -0700

    Completed Array Challenge 2

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,13 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array) {
+var secondArray = array.reverse();
+secondArray.unshift(1);
+return secondArray;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(array) { 
+  return array.join( + );
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(array) {
+  return array.length + 1;
+};
