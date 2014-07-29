7c29abacf2e3a52f7654e43a245013411f798be8
Garrett Sussman <gasussman@gmail.com>
2014-07-29 09:45:10 -0700

    Completed Array Functions

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,9 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(num1, num2, num3, num4) {
+  var firstArray = [num1, num2, num3, num4];
+  return firstArray;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(array) {
+  var secondArray = [array.shift(), array.pop();]
+  return secondArray;
+};
