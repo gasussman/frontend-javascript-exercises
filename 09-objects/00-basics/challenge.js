491e4e94a7ca9f765702532de6a1bdfff7a08f7b
Garrett Sussman <gasussman@gmail.com>
2014-07-29 15:44:01 -0700

    Completed Object challenges

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,30 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
+  return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  object[newProp] = newValue;
+  return object;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(newProps) {
+  var message = newProps;
+  var messageTogether = "Hello " + message.recipient + ",\n\n" + message.msg + "\n\nSincerely,\n" + message.sender;
+  return messageTogether;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  var price = {
+    "MacBook Air": 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1
+  };
+  var myPrice = price[item];
+  return money >= myPrice;
+};
