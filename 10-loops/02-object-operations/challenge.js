581cc7315fd3e6a835db516b0e345d5f5b0c4f83
Garrett Sussman <gasussman@gmail.com>
2014-07-30 12:05:44 -0700

    Completed Loops Challenge

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,25 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object) {
+  var monster = {};
+  for (var property in object) {
+    monster[property] = object[property];
+  }
+  return monster;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+  for (var property in src) {
+    dest[property] = src[property];
+  }
+  return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(object, array) {
+
+return !array.some(function(item){return !(item in object)})
+    /*for (var i = 0; i < array.length; i++) {
+        if (!(array[i] in object)) {return false;}
+
+    }
+    return true;
+  */
+};
