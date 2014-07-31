7cef8fcb834967287114d8a927ba584960440589
Garrett Sussman <gasussman@gmail.com>
2014-07-30 18:19:08 -0700

    completed looping over array functions

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,25 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+  var sum = 0; 
+  for(var i = 0; i < array.length; i++) {
+    sum += array[i];
+  };
+  return sum;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function (inputString) {
+  var cutUp = inputString.split(",");
+  var mashUp = [];
+  for (var i = 0; i < cutUp.length; i++) {
+    mashUp.push(cutUp[i].toLowerCase());
+  }
+  return mashUp;
 
-module.exports.addIndex = undefined;
+};
+
+module.exports.addIndex = function (array) {
+  var oneUp = [];
+  for (var i = 0; i < array.length; i++) {
+    oneUp.push(i + " is " + array[i]);
+  }
+  return oneUp;
+};
