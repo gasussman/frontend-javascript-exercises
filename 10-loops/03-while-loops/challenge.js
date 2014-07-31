196e41c6a8fbc1391e650a8fc89bad0fed6fa666
Garrett Sussman <gasussman@gmail.com>
2014-07-30 18:16:19 -0700

    Completed while loops

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,19 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn) {
+  while (conditionalFn()) {
+    actionFn();
+  }
+};
 
-module.exports.sumNumbers = undefined;
+
+module.exports.sumNumbers = function(array) {
+  if(array.length === 0) {return 0;}
+
+  var i = 0;
+  var result = 0;
+  while (i < array.length) {
+    result += array[i];
+    i++;
+  }
+  return result;
+};
 
